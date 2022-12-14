ae693ce0c82aa55693bf4ea01f66dcd20876c260
June Balter <junedoeswork@gmail.com>
2022-12-14 15:29:13 -0500

    Calculator with all functions working

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,42 @@
 
 def add(num1, num2):
     """Return the sum of num1 and num2."""
-
-    return 10
+    sum = num1 + num2
+    return sum
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
-
+    sub = num1 - num2
+    return sub
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
-
+    product = num1 * num2
+    return product
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
-
+    division = num1 / num2
+    return division
 
 def square(num1):
     """Return the square of num1."""
-
+    sq = num1 * num1
+    return sq
 
 def cube(num1):
     """Return the cube of num1."""
-
+    cb = num1 * num1 * num1
+    return cb
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    pw = num1**num2
+    return pw
 
 def mod(num1, num2):
+
+    md = num1 % num2 
+    return md
     """Return the remainder of num1 / num2."""
